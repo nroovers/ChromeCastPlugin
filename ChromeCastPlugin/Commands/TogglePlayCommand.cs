801589b5e9cc29c6460cb943f66492771ccd849c
Nicolai Roovers <81565980+nicolai-roovers-ld@users.noreply.github.com>
2022-09-23 15:40:10 +0300

    TogglePlay as Multistate profile action

--- ChromeCastPlugin/Commands/TogglePlayCommand.cs
+++ ChromeCastPlugin/Commands/TogglePlayCommand.cs
@@ -1,43 +1,49 @@
 ﻿namespace Loupedeck.ChromeCastPlugin.Commands
 {
     using System;
-
     using Loupedeck.ChromeCastPlugin.ChromeCastWrapper;
 
-    internal class TogglePlayCommand : PluginDynamicCommand
-    {
-        // Examples:
-        // http://icecast.vrtcdn.be/stubru-high.mp3 
-        // http://stream.radiohelsinki.fi:8002/;?type=http&nocache=13776
+    // Examples:
+    // http://icecast.vrtcdn.be/stubru-high.mp3 
+    // http://stream.radiohelsinki.fi:8002/;?type=http&nocache=13776
 
+    internal class TogglePlayCommand : PluginMultistateDynamicCommand
+    {
         public TogglePlayCommand() : base(
             "Play",
             "Select, play and pause online media",
             "")
-            => this.MakeProfileAction("text;URL of media to cast");
+        {
+            this.AddState("Pause", "Pause");
+            this.AddState("Play", "Play");
+            this.MakeProfileAction("text;URL of media to cast");
+        }
 
         private ChromeCastPlugin ChromeCastPlugin => this.Plugin as ChromeCastPlugin;
-        private IChromeCastWrapper ChromeCastWrapper => this.ChromeCastPlugin.ChromeCastApi;
 
         #region PluginDynamicCommand overrides
+
         protected override void RunCommand(String actionParameter)
         {
-            if (this.ChromeCastWrapper == null)
+            if (this.ChromeCastPlugin.ChromeCastApi == null)
             {
                 return;
             }
 
             try
             {
-                if (this.ChromeCastWrapper.PlayBackUrl != actionParameter ||
-                    this.ChromeCastWrapper.PlayBackState != PlayBackState.Playing)
+                if (this.ChromeCastPlugin.ChromeCastApi.PlayBackUrl != actionParameter ||
+                    this.ChromeCastPlugin.ChromeCastApi.PlayBackState != PlayBackState.Playing)
                 {
-                    this.ChromeCastWrapper.PlayCast(actionParameter);
+                    this.ChromeCastPlugin.ChromeCastApi.PlayCast(actionParameter);
+                    this.SetCurrentState(actionParameter, 1);
                 }
                 else
                 {
-                    this.ChromeCastWrapper.PauseCast();
+                    this.ChromeCastPlugin.ChromeCastApi.PauseCast();
+                    this.SetCurrentState(actionParameter, 0);
                 }
+                this.ActionImageChanged(actionParameter);
             }
             catch (Exception e)
             {
